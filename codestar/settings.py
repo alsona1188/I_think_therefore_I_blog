ceea95c1b20c31fb879577cd7e0d791afc922673
Alsona Natsi <alsona.naci1188@gmail.com>
2024-05-23 21:49:00 +0000

    Formating the Post and the comment with methods

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=9@4i%!o09i^par+%_u7-acc3r9xl)xe7du2^^3iion^c+ix$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alsona1188-ithinktheref-ve7soob3ddi.ws-eu114.gitpod.io','.herokuapp.com']
 
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
