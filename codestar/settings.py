6da3e9a4eea36d4f1a6529f2090b2af9fdf3f704
Alsona Natsi <alsona.naci1188@gmail.com>
2024-05-28 19:53:23 +0000

    Ready to deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 SECRET_KEY = 'django-insecure-=9@4i%!o09i^par+%_u7-acc3r9xl)xe7du2^^3iion^c+ix$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-alsona1188-ithinktheref-ve7soob3ddi.ws-eu114.gitpod.io','.herokuapp.com']
 
