7697c57fa47a979b0e90baea4c511ec67ea83924
Alsona Natsi <alsona.naci1188@gmail.com>
2024-06-04 04:36:58 +0000

    Authomated testing

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import sys
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import dj_database_url
@@ -109,6 +110,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
